cc36e72e2e6438eb7e7cd4278187f7bbf41e26e7
notmyf4ulty <notmyf4ulty@github.com>
2016-08-20 14:21:48 +0200

    Added some Main's code. (not working :/).

--- src/com/catnbearsoftware/Main.java
+++ src/com/catnbearsoftware/Main.java
@@ -1,10 +1,18 @@
 package com.catnbearsoftware;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Created by przemek on 20.08.16.
  */
 public class Main {
     public static void main(String [] args) {
-        
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MyDatabase");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
