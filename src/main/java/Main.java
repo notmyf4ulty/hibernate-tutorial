a2e0b1c8d794602e1ec4c29cc6797d802a94c39e
notmyf4ulty <notmyf4ulty@github.com>
2016-08-25 22:51:29 +0200

    Added code which creates a first enitity.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import domain.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,6 +11,16 @@ public class Main {
     public static void main(String [] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MyDatabase");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setFirstName("Jan");
+        employee.setLastName("Kowalski");
+        employee.setSalary(3333.3);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
+        entityManager.getTransaction().commit();
 
         entityManager.close();
         entityManagerFactory.close();
