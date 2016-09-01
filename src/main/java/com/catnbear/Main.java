4ade7478635d766d9a5c17c1b16e3a8f8aea5b4b
notmyf4ulty <notmyf4ulty@github.com>
2016-09-01 23:49:36 +0200

    Adds table rows to the listview in entry level GUI.

--- src/main/java/com/catnbear/Main.java
+++ src/main/java/com/catnbear/Main.java
@@ -2,36 +2,51 @@ package com.catnbear;
 
 import com.catnbear.database.Budget;
 import com.catnbear.database.DatabaseConnector;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.sql.*;
 import java.util.*;
-import java.util.Date;
 
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/rootlayout.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] argv) {
 
-        DatabaseConnector connector = DatabaseConnector.getInstance();
-        Connection connection = connector.getConnection();
-        ResultSet resultSet = null;
-        Vector<Budget> budgetVector = new Vector<Budget>();
-        try {
-            Statement statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM wimm.notmyf4ulty_budget;");
-            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-            System.out.println(Budget.validateTable(resultSetMetaData));
+//        DatabaseConnector connector = DatabaseConnector.getInstance();
+//        Connection connection = connector.getConnection();
+//        ResultSet resultSet = null;
+//        Vector<Budget> budgetVector = new Vector<Budget>();
+//        try {
+//            Statement statement = connection.createStatement();
+//            resultSet = statement.executeQuery("SELECT * FROM wimm.notmyf4ulty_budget;");
+//            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//            System.out.println(Budget.validateTable(resultSetMetaData));
+//
+//            while (resultSet.next()) {
+//                budgetVector.add(new Budget(resultSet));
+//            }
+//            for (Budget i : budgetVector) {
+//                System.out.println(i);
+//            };
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 
-            while (resultSet.next()) {
-                budgetVector.add(new Budget(resultSet));
-            }
-            for (Budget i : budgetVector) {
-                System.out.println(i);
-            };
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-
+        launch(argv);
     }
 }
---
+++ src/main/java/com/catnbear/fxml/StartLayoutController.java
@@ -0,0 +1,51 @@
+package com.catnbear.fxml;
+
+import com.catnbear.database.Budget;
+import com.catnbear.database.DatabaseConnector;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * Created by przemek on 01.09.16.
+ */
+public class StartLayoutController {
+    @FXML private ListView listView;
+
+    @FXML private void onButtonAction() {
+        DatabaseConnector connector = DatabaseConnector.getInstance();
+        Connection connection = connector.getConnection();
+        ResultSet resultSet = null;
+        Vector<Budget> budgetVector = new Vector<Budget>();
+        try {
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM wimm.notmyf4ulty_budget;");
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            System.out.println(Budget.validateTable(resultSetMetaData));
+
+            while (resultSet.next()) {
+                budgetVector.add(new Budget(resultSet));
+            }
+            for (Budget i : budgetVector) {
+                System.out.println(i);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ObservableList<Budget> list = FXCollections.observableArrayList(new ArrayList<Budget>(budgetVector));
+        listView.setItems(list);
+    }
+
+    public ListView getListView() {
+        return listView;
+    }
+
+    public void setListView(ListView listView) {
+        this.listView = listView;
+    }
+}
