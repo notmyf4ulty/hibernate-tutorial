57108ff78782bf682f4680e9f3c695796ec78bc3
notmyf4ulty <notmyf4ulty@github.com>
2016-09-18 19:41:26 +0200

    Filters are shown on a list in a filters window.

--- src/main/java/com/catnbear/fxml/FiltersController.java
+++ src/main/java/com/catnbear/fxml/FiltersController.java
@@ -1,21 +1,18 @@
 package com.catnbear.fxml;
 
-import com.catnbear.database.Budget;
-import com.catnbear.database.DataFilter;
 import com.catnbear.database.DataModel;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.sql.SQLException;
 
 public class FiltersController {
-    @FXML private ComboBox yearComboBox;
-    @FXML private ComboBox monthComboBox;
-    @FXML private ComboBox categoryComboBox;
     @FXML private TextField categoryTextField;
     @FXML private TextField valueTextField;
+    @FXML
+    private ListView filtersListView;
     private DataModel dataModel;
 
     @FXML
@@ -23,29 +20,7 @@ public class FiltersController {
         dataModel = DataModel.getInstance();
         dataModel.getDataFilter().setDatabaseName("wimm");
         dataModel.getDataFilter().setTableName("notmyf4ulty_budget");
-        try {
-            categoryComboBox.setItems(DataModel.getInstance().getAllCategories());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML private void yearChoiceBoxChosen() {
-    }
-
-    @FXML private void monthChoiceBoxChosen() {
-
-    }
-
-    @FXML private void categoryChoiceBoxChosen() {
-        try {
-            dataModel.filterData(
-                    "category",
-                    categoryComboBox.getSelectionModel().getSelectedItem().toString()
-            );
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        updateFiltersListView();
     }
 
     @FXML
@@ -63,6 +38,7 @@ public class FiltersController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        updateFiltersListView();
     }
 
     @FXML
@@ -76,6 +52,16 @@ public class FiltersController {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        updateFiltersListView();
+    }
+
+    private void updateFiltersListView() {
+        filtersListView.setItems(
+                FXCollections.observableArrayList(
+                        dataModel
+                                .getDataFilter()
+                                .getAllFilters()
+                                .toArray()));
     }
 }
 
