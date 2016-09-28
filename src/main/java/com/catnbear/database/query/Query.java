5c5ccd19b1123d89f74e9a9e8b077b1c6472e6ab
notmyf4ulty <notmyf4ulty@github.com>
2016-09-28 13:37:04 +0200

    Added nnew Query methods.

--- src/main/java/com/catnbear/database/query/Query.java
+++ src/main/java/com/catnbear/database/query/Query.java
@@ -1,6 +1,7 @@
 package com.catnbear.database.query;
 
 import com.catnbear.database.DatabaseConnector;
+import com.catnbear.database.filter.FiltersList;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -34,13 +35,76 @@ class Query {
     public ResultSet selectAll() {
         DatabaseConnector connector = DatabaseConnector.getInstance();
         ResultSet resultSet = null;
+        String query = "SELECT * FROM " + databaseName + "." + tableName + ";";
         try {
-            connector.executeReadQuery("SELECT * FROM " + databaseName + "." + tableName);
+            resultSet = connector.executeReadQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return resultSet;
     }
+
+    public ResultSet select(FiltersList filtersList) {
+        DatabaseConnector connector = DatabaseConnector.getInstance();
+        ResultSet resultSet = null;
+        String query = "SELECT * FROM " + databaseName + "." + tableName + " "
+                + "WHERE " + filtersList.toQueryString() + ";";
+        try {
+            resultSet = connector.executeReadQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public int add(List<String> columnsList, List<String> valuesList) {
+        String query = "INSERT INTO " + databaseName + "." + tableName + " ";
+
+        if (!(columnsList.isEmpty() || (columnsList == null))) {
+            query += "(";
+            for (String column : columnsList) {
+                query += column;
+                if (columnsList.indexOf(column) < (columnsList.size() - 1)) {
+                    query += ",";
+                }
+            }
+            query += ") ";
+        } else {
+            query += " ";
+        }
+
+        query += "VALUES ";
+
+        if (!(valuesList.isEmpty() || (valuesList == null))) {
+            query += "(";
+            for (String column : valuesList) {
+                query += column;
+                if (valuesList.indexOf(column) < (valuesList.size() - 1)) {
+                    query += ",";
+                }
+            }
+            query += ")";
+        } else {
+            query += "";
+        }
+
+        query += ";";
+
+        DatabaseConnector connector = DatabaseConnector.getInstance();
+        int affectedRows = -1;
+        try {
+            affectedRows = connector.executeWriteQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return affectedRows;
+    }
+
+//    public int remove(String columnName, String columnValue) {
+//        String query = "REMOVE FROM " + databaseName + "." + tableName + " WHERE "
+//                + "\""
+//    }
 
     public String toExecutableString() {
         return "";
