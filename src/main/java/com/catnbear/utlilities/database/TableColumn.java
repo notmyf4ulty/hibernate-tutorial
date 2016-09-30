220946074e58134de98ff224ee1e72161e7cabb0
notmyf4ulty <notmyf4ulty@github.com>
2016-09-30 17:42:09 +0200

    Added new field and new constructor to TableColumn.

--- src/main/java/com/catnbear/utlilities/database/TableColumn.java
+++ src/main/java/com/catnbear/utlilities/database/TableColumn.java
@@ -2,6 +2,7 @@ package com.catnbear.utlilities.database;
 
 public class TableColumn {
     private String columnName;
+    private String displayedColumnName;
     private int tableIndex;
     private int resultSetIndex;
 
@@ -11,12 +12,25 @@ public class TableColumn {
         this.resultSetIndex = tableIndex + 1;
     }
 
+    public TableColumn(String columnName, String displayedColumnName, int tableIndex) {
+        this(columnName, tableIndex);
+        this.displayedColumnName = displayedColumnName;
+    }
+
     public String getColumnName() {
         return columnName;
     }
 
     public void setColumnName(String columnName) {
         this.columnName = columnName;
+    }
+
+    public String getDisplayedColumnName() {
+        return displayedColumnName;
+    }
+
+    public void setDisplayedColumnName(String displayedColumnName) {
+        this.displayedColumnName = displayedColumnName;
     }
 
     public int getTableIndex() {
